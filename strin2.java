7b15ff0fd73b41fe403db84c6b8fcf83b3c3cb03
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-05 16:53:01 +0000

    string q2

---
+++ strin2.java
@@ -0,0 +1,10 @@
+package how.flowchartexamplejava;
+
+public class strin2 {
+	public static void main(String[] args) {
+		String s = "Hello There";
+		for (int i = 0; i < s.length(); i++) {
+		    System.out.println(s.charAt(i));
+		}
+	}
+}
--- string1.java
+++ string1.java
@@ -2,26 +2,11 @@ package how.flowchartexamplejava;
 
 public class string1 {
 	public static int wordcount(String s){
-		boolean wordsolv = false;
-	    int wCount = 0;
+		String s1 = "someString";
+		
+		for (int i = 0; i < s1.length(); i++) {
+		    System.out.println(s1.charAt(i));
+		}
 
-	    
-	    int end = s.length() - 1;
-
-	    for (int i = 0; i < s.length(); i++) {
-	        if (Character.isLetter(s.charAt(i)) && i != end) {
-	            wordsolv = true;
-	        } else if (!Character.isLetter(s.charAt(i)) && wordsolv) {
-	            wCount++;
-	            wordsolv = false;
-	        } else if (Character.isLetter(s.charAt(i)) && i == end) {
-	            wCount++;
-	        }
-	    }
-	    return wCount;
-	}
-	public static void main(String[] args) {
-		System.out.println(wordcount("No, I am your father."));
-	}
-
+}
 }
