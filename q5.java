40cf93cb67401e56886188cdf8c91f16f7cf2be1
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-05 13:49:38 +0000

    exercise 5 updated

---
+++ q5.java
@@ -0,0 +1,14 @@
+package how.flowchartexamplejava;
+
+public class q5 {
+	
+	public static void main(String[] args) {
+		int num = 1;
+		for(num = 1; num<11; num++) {
+			for(int i =0; i<num;i++) {
+				System.out.println(num);
+			}
+		}
+
+	}}
+		
