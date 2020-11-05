86cad5cc28c15a7278b59d0ee5250ea680ead60d
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-05 13:07:33 +0000

    exercise 1 and 2

---
+++ ex.java
@@ -0,0 +1,18 @@
+package how.flowchartexamplejava;
+
+public class ex {
+	
+	public static void main(String[] args) {
+		int a = 100;
+		while(a <= 200) {
+			a++;
+			System.out.println(a);
+		}
+		
+		
+			}
+		
+		
+	
+
+}
---
+++ q2.java
@@ -0,0 +1,22 @@
+package how.flowchartexamplejava;
+
+public class q2 {
+	
+	public static void main(String[] args) {
+		int a = 100;
+		while(a <= 200) {
+			if(a%2==0) {
+				System.out.print("*");
+			} else {
+				System.out.print("-");
+			}
+			a++;
+		}
+		
+		
+			}
+		
+		
+	
+
+}
