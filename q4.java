a51f983bde633fedd7dd04a865a1df24fc1aedf1
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-05 13:38:23 +0000

    exercise 4 updated

--- q4.java
+++ q4.java
@@ -5,14 +5,18 @@ public class q4 {
 	public static void main(String[] args) {
 		int num = 1;
 		int numCount =0;
+		while(numCount < 11) {
+			
+		
 		for(num = 1; num<11; num++) {
 			System.out.println(num);
-			if(num == 11) {
-				numCount ++;
-				num = 1;
+			
+		}
+		num = 1;
+		numCount ++;
 		}
 		}
-	}}
+	}
 		
 		
 
