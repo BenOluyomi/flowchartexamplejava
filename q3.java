9e5822abf353cbf001507e9598f3d43a4af1d411
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-05 13:19:20 +0000

    exercise 3

---
+++ q3.java
@@ -0,0 +1,23 @@
+package how.flowchartexamplejava;
+
+public class q3 {
+	
+	public static void main(String[] args) {
+		int num = 1;
+		int numCount =0;
+		while(numCount< 10) {
+			System.out.println(num);
+			num++;
+			if(num == 11) {
+				numCount ++;
+				num = 1;
+		} else {
+			
+		}
+		
+		
+			}
+	}}
+		
+		
+
