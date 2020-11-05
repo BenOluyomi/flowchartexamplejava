a21826fe886d2b4ad8b57f04d0c3ed446445aed4
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-05 15:51:32 +0000

    coins updated again

--- coins.java
+++ coins.java
@@ -5,7 +5,7 @@ public class coins {
 	
 	public static void main(String[] args) {
 		  int payed = 2000;
-		  int cost = 454;
+		  int cost = 252;
 		  
 		  int change = payed - cost;
 		  
@@ -13,20 +13,33 @@ public class coins {
 		  int newc = payed - cost - tens*1000;
 		  int fives = (newc -change%500)/500;
 		  int newco = newc -fives*500;
-		  int twentyp = (newco -change%20)/20;
-		  int newcos = newco -twentyp*20;
+		  
+		  int pounds = (newco -change%100)/100;
+		  int newcoi = newco -pounds*100;
+		  
+		  int fiftyp = (newcoi -change%50)/50;
+		  int newcoin = newcoi -fiftyp*50;
+		  
+		  int twentyp = (newcoin -change%20)/20;
+		  int newcos = newcoin -twentyp*20;
+		  
 		  int tenp = (newcos -change%10)/10;
 		  int newcost = newcos -tenp*10;
 		  int two = (newcost -change%2)/2;
 		  int newcosts = newcost -two*2;
 		  int one = (newcosts -change%1)/1;
 		  int newcostsi = newcosts -one*1;
+		  double accchange = ((double)payed-(double)cost)/100;
 		  
 		  System.out.println(tens+ " 10 pound notes");
 		  System.out.println(fives+ " 5 pound notes");
+		  System.out.println(pounds+ " £1 coins");
+		  System.out.println(fiftyp+ " 50ps");
 		  System.out.println(twentyp+ " 20ps");
-		  System.out.println(one+ " 2ps");
+		  System.out.println(tenp+ " 10ps");
+		  System.out.println(two+ " 2ps");
 		  System.out.println(one+ " 1ps");
+		  System.out.println("Your change is £"+accchange);
 	}
 
 }
