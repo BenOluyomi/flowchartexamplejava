b19ab324c8b0bbe27252b05055670fad2bbedfe9
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-05 13:25:02 +0000

    exercise 4

---
+++ q4.java
@@ -0,0 +1,18 @@
+package how.flowchartexamplejava;
+
+public class q4 {
+	
+	public static void main(String[] args) {
+		int num = 1;
+		int numCount =0;
+		for(num = 1; num<11; num++) {
+			System.out.println(num);
+			if(num == 11) {
+				numCount ++;
+				num = 1;
+		}
+		}
+	}}
+		
+		
+
