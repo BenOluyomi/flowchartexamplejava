3baacc97b3eaf958c9d52a63956b7082d3abff42
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-06 09:19:25 +0000

    string q1 fix

--- string1.java
+++ string1.java
@@ -1,12 +1,31 @@
 package how.flowchartexamplejava;
 
 public class string1 {
-	public static int wordcount(String s){
-		String s1 = "someString";
-		
-		for (int i = 0; i < s1.length(); i++) {
-		    System.out.println(s1.charAt(i));
-		}
+	public static int countWords(String s){
+
+	    int wCount = 0;
+
+	    boolean isword = false;
+	    int end = s.length() - 1;
+
+	    for (int i = 0; i < s.length(); i++) {
+	 
+	        if (Character.isLetter(s.charAt(i)) && i != end) {
+	            isword = true;
+	     
+	        } else if (!Character.isLetter(s.charAt(i)) && isword) {
+	            wCount++;
+	            isword = false;
+	            
+	        } else if (Character.isLetter(s.charAt(i)) && i == end) {
+	            wCount++;
+	        }
+	    }
+	    return wCount;
+	}
+	public static void main(String[] args) {
+		String word = "Hello there";
+		countWords(word);
 
 }
 }
