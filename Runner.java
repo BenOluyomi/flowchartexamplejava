3d55ef4108642ea6ef03d8d0523e8f40c8c19ce1
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-06 11:32:00 +0000

    string array print

---
+++ Runner.java
@@ -0,0 +1,13 @@
+package how.flowchartexamplejava;
+
+public class Runner {
+
+	
+
+    public static void main(String[] args) {
+    String words[] = {"hello","there","mate","i'm","over","here"};	
+        for(String i : words) {
+            System.out.println(i);
+        }
+    }
+}
